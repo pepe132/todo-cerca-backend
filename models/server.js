2d6ce325be23ed5e627d75c0d9304cc3a59545f6
pepe132 <jose.garciadom4gmail.com>
2022-12-14 17:17:34 -0600

    Fix port

--- models/server.js
+++ models/server.js
@@ -5,6 +5,7 @@ const { dbConnection } = require('../database/config');
 class Server{
     constructor(){
         this.app=express();
+        this.port=process.env.PORT
         this.paths={
             usuarios:'/api/usuarios',
             auth:'/api/auth',
