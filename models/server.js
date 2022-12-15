790920c6e7e3827979f471f2889bc7a38f216ea0
pepe132 <jose.garciadom4gmail.com>
2022-12-14 21:36:04 -0600

    Change port to production

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ const { dbConnection } = require('../database/config');
 class Server{
     constructor(){
         this.app=express();
-        this.port=process.env.PORT || 8082;
+        this.port=process.env.PORT || 8081 
         this.paths={
             usuarios:'/api/usuarios',
             auth:'/api/auth',
